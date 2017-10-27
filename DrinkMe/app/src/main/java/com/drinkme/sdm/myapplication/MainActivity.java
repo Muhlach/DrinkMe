ca372b282c3dba1a1633dc5f1bfd37f022313546
sergiosantano <sergiosantano96@gmail.com>
2017-10-27 19:22:47 +0200

    inicio de trabajo con la main activity

--- DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/MainActivity.java
+++ DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/MainActivity.java
@@ -1,13 +1,75 @@
 package com.drinkme.sdm.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_beber);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        setInitialFragment();
     }
+
+    /**
+     * Metodo que te carga al fragment en funcion de lo que seleccionas en la navigation bar
+     */
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_logros:
+                    fragment = new LogrosFragment();
+                    break;
+                case R.id.navigation_beber:
+                    fragment = new BeberFragment();
+                    break;
+                case R.id.navigation_estadisticas:
+                    fragment = new EstadisticasFragment();
+                    break;
+            }
+            replaceFragment(fragment);
+            return true;
+        }
+
+    };
+
+    /**
+     * Carga el fragment principal (el de beber)
+     */
+    private void setInitialFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_fragment_placeholder, new BeberFragment());
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Recibe un fragmento que cargará en la pantalla que se visualiza
+     * @param fragment que mostrará en pantalla
+     */
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment_placeholder, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
