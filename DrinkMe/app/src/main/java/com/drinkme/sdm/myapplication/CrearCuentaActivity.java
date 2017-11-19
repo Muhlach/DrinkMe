bbb49628fe52e174dc151fac67dd508e4ede55c7
alex <alesalga@ucm.es>
2017-11-19 22:35:55 +0100

    actualizacion general

--- DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/CrearCuentaActivity.java
+++ DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/CrearCuentaActivity.java
@@ -3,13 +3,84 @@ package com.drinkme.sdm.myapplication;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class CrearCuentaActivity extends AppCompatActivity {
+
+    Spinner spinnerSex;
+    Spinner spinnerAltura;
+    Spinner spinnerPeso;
+    Spinner spinnerDia;
+    Spinner spinnerMes;
+    Spinner spinnerAno;
+    EditText nombre_et, apellidos_et, nombreDeUo_et, correo_et, password_et, repassword_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-    }
+
+        nombre_et = (EditText) findViewById(R.id.editTextNombre);
+        apellidos_et = (EditText) findViewById(R.id.editTextApellidos);
+        correo_et = (EditText) findViewById(R.id.editTextCorreo);
+        password_et = (EditText) findViewById(R.id.editTextPassword);
+        repassword_et = (EditText) findViewById(R.id.editTextRePassword);
+
+
+        spinnerSex = (Spinner) findViewById(R.id.spinnerSex);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.sex_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSex.setAdapter(adapter);
+
+        spinnerAltura = (Spinner) findViewById(R.id.spinnerAltura);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.altura_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAltura.setAdapter(adapter2);
+
+        spinnerPeso = (Spinner) findViewById(R.id.spinnerPeso);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,R.array.peso_array, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPeso.setAdapter(adapter3);
+
+        spinnerDia = (Spinner) findViewById(R.id.spinnerDia);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,R.array.dia_array, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDia.setAdapter(adapter4);
+
+        spinnerMes = (Spinner) findViewById(R.id.spinnerMes);
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,R.array.mes_array, android.R.layout.simple_spinner_item);
+        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMes.setAdapter(adapter5);
+
+        spinnerAno = (Spinner) findViewById(R.id.spinnerAno);
+        ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this,R.array.ano_array, android.R.layout.simple_spinner_item);
+        adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAno.setAdapter(adapter6);
+
+         }
+
+         public void onClickCrearCuenta(View v){
+
+            /* String nombre, apellidos, usuario, password, repassword, correo;
+             nombre = nombre_et.getText().toString();
+             apellidos = apellidos_et.getText().toString();
+             usuario = nombreDeUo_et.getText().toString();
+             password = password_et.getText().toString();
+             repassword = repassword_et.getText().toString();
+             correo = correo_et.getText().toString();
+
+             if(nombre == null || apellidos == null || usuario == null || password == null || repassword == null || correo == null)*/
+                 Toast.makeText(getApplicationContext(),"Completa todos los campos",Toast.LENGTH_SHORT);
+
+             /*if(password != repassword)
+                 Toast.makeText(getApplicationContext(),"Comprueba tu password",Toast.LENGTH_SHORT);*/
+
+
+
+         }
 }
--- DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/LoginActivity.java
+++ DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/LoginActivity.java
@@ -1,19 +1,58 @@
 package com.drinkme.sdm.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText user_et;
+    private EditText password_et;
+    private SharedPreferences mSharedPreferences;
+    private boolean holdSesion;
+    private CheckBox checkBox;
+    private boolean debug = true;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+         mSharedPreferences =
+                getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+
+        String user = mSharedPreferences.getString ("user", null);
+
+        String password = mSharedPreferences.getString ("password", null);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        if(user == null || password == null){
+            setContentView(R.layout.activity_login);
+            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            user_et = (EditText) findViewById(R.id.editTextUser);
+            password_et = (EditText) findViewById(R.id.editTextPassword);
+            checkBox = (CheckBox) findViewById(R.id.checkBoxMantenerSesion);
+        }else {
+            if(debug)
+            deleteSharedPreferences();
+            finish();
+            launchMainActivity();
+        }
     }
+
+    private void launchMainActivity(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
 
     public void botonCrearCuentaOnClick(View v){
         Intent intent = new Intent(LoginActivity.this, CrearCuentaActivity.class);
@@ -21,11 +60,37 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void botonEntrarOnClick(View v){
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
+
+        String user = user_et.getText().toString();
+        String password = password_et.getText().toString();
+        holdSesion = checkBox.isChecked();
+
+        if(checkUserAndPassword(user, password)){
+            if(holdSesion)
+            saveInSharedPreferences(user, password);
+            launchMainActivity();
+        }
+        else
+            Toast.makeText(getApplicationContext(),"Datos incorrectos", Toast.LENGTH_SHORT);
     }
 
+    private boolean checkUserAndPassword(String user, String password){
+        return true;
+    }
 
+    private void saveInSharedPreferences(String user, String password){
+        final SharedPreferences.Editor mEditor =
+                mSharedPreferences.edit();
+        mEditor.putString("user", user);
+        mEditor.putString("password",password);
+        mEditor.commit();
+    }
 
-
+    private void deleteSharedPreferences(){
+        final SharedPreferences.Editor mEditor =
+                mSharedPreferences.edit();
+        mEditor.putString("user", null);
+        mEditor.putString("password",null);
+        mEditor.commit();
+    }
 }
