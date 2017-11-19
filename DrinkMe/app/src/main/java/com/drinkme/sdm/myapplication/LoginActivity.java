6f797f55c18e5d6b229cb5b872aee5f0b6d9ecc9
alex <alesalga@ucm.es>
2017-11-19 18:33:25 +0100

    alineacion en crear cuenta

--- DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/LoginActivity.java
+++ DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.drinkme.sdm.myapplication;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     public void botonCrearCuentaOnClick(View v){
