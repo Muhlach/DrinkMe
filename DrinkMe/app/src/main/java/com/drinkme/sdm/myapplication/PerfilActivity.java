834e257d42428f8ea0a6f365e3aaa6bd8e5990f9
alex <alesalga@ucm.es>
2017-11-29 13:02:22 +0100

    avanzo en perfil activity

--- DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/PerfilActivity.java
+++ DrinkMe/app/src/main/java/com/drinkme/sdm/myapplication/PerfilActivity.java
@@ -1,13 +1,31 @@
 package com.drinkme.sdm.myapplication;
 
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.drinkme.sdm.myapplication.logic.Usuario;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.drinkme.sdm.myapplication.R.id.itemEditarPassword;
 
 public class PerfilActivity extends AppCompatActivity {
+
+
+    private Spinner spinnerAltura;
+    private Spinner spinnerPeso;
+    private Usuario user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +35,69 @@ public class PerfilActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_perfil);
 
+        Bundle bundle = getIntent().getExtras();
 
+        user = bundle.getParcelable(MainActivity.USER_KEY);
+
+        findViews();
+
+      //  Toast.makeText(getApplicationContext(),String.valueOf(user_p.getAltura()),Toast.LENGTH_LONG).show();
 
     }
 
+
+    private void findViews(){
+
+        spinnerAltura = (Spinner) findViewById(R.id.spinnerAlturaP);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.altura_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAltura.setAdapter(adapter2);
+
+      List<String> list = Arrays.asList(getResources().getStringArray(R.array.altura_array));
+
+
+       int i = 0;
+       for(i = 0; i < list.size(); i++){
+           if(list.get(i).equalsIgnoreCase(String.valueOf(user.getAltura())))
+               break;
+       }
+
+        spinnerAltura.setSelection(i);
+        spinnerAltura.setEnabled(true);
+
+
+              spinnerAltura.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                  @Override
+                  public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                      Toast.makeText(getApplicationContext(),"ke",Toast.LENGTH_LONG).show();
+                  }
+
+                  @Override
+                  public void onNothingSelected(AdapterView<?> adapterView) {
+
+                  }
+              });
+
+
+
+
+        spinnerPeso = (Spinner) findViewById(R.id.spinnerPesoP);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.peso_array, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPeso.setAdapter(adapter3);
+
+
+        List<String> list2 = Arrays.asList(getResources().getStringArray(R.array.peso_array));
+
+        int j = 0;
+        for(j = 0; j < list2.size(); j++){
+            if(list2.get(j).equalsIgnoreCase(String.valueOf(user.getPeso())))
+                break;
+        }
+
+        spinnerPeso.setSelection(j);
+        spinnerPeso.setEnabled(false);
+    }
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
